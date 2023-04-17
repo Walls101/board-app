70a21656d54c8cf17c5285d9b9ee5c7b58185059
Alex Paredes <alexparedes83@gmail.com>
2023-04-17 10:11:27 -0600

    search page completed

--- src/app/modules/material.module.ts
+++ src/app/modules/material.module.ts
@@ -7,6 +7,7 @@ import {MatIcon, MatIconModule} from '@angular/material/icon'
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
 
 
 
@@ -20,6 +21,7 @@ import { MatInputModule } from '@angular/material/input';
     MatIconModule,
     MatInputModule,
     MatButtonModule,
+    MatCardModule,
   ],
   exports: [
     MatToolbarModule,
@@ -28,6 +30,7 @@ import { MatInputModule } from '@angular/material/input';
     MatIconModule,
     MatInputModule,
     MatButtonModule,
+    MatCardModule,
   ]
 })
 export class MaterialModule { }
